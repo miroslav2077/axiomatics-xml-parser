ad473a2144e6bba47f9b1a2326793b04a185340e
miroslav2077 <91072960+miroslav2077@users.noreply.github.com>
2022-05-23 22:34:23 +0200

    formatting

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-import './App.css';
+import "./App.css";
 
-import TreeNode from './components/TreeNode';
+import TreeNode from "./components/TreeNode";
 
-import { XMLParser } from 'fast-xml-parser';
+import { XMLParser } from "fast-xml-parser";
 
 function App() {
-
   const fr = new FileReader();
 
   // xmlparser options
   const options = {
-    ignoreAttributes : false
+    ignoreAttributes: false,
   };
 
   const parser = new XMLParser(options);
 
   // initialize xml string as empty
-  const [ selectedFile, setSelectedFile ] = useState('');
-  
-  const [ xmlObject, setXmlObject ] = useState(null);
+  const [selectedFile, setSelectedFile] = useState("");
+
+  const [xmlObject, setXmlObject] = useState(null);
 
   const onSelectedFileChange = (e) => {
     // file upload
     fr.onload = (loadedFile) => {
       setSelectedFile(loadedFile.target.result);
-    }
+    };
     fr.readAsText(e.target.files[0]);
-  }
+  };
 
   useEffect(() => {
     const obj = parser.parse(selectedFile);
@@ -38,9 +37,7 @@ function App() {
   return (
     <div className="container mx-auto px-4 bg-slate-600">
       <div className="flex flex-col space-y-8">
-        <h1 className="text-3xl font-bold">
-          XML Tree Visualizer
-        </h1>
+        <h1 className="text-3xl font-bold">XML Tree Visualizer</h1>
         <input type="file" onChange={onSelectedFileChange} />
 
         <TreeNode xmlObject={xmlObject} />
